bc055a86cd62181d0aa6c274dcaa6e78d95f8ee3
Young Cho <youngtae1386@gmail.com>
2019-03-15 18:49:43 -0500

    checking in HW into GIThub

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+      name: "Spider Monkey",
+      photo:
+        "https://www.monkeyworlds.com/wp-content/uploads/spider_monkey.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Squirrel Monkey",
+      photo:
+        "https://www.monkeyworlds.com/wp-content/uploads/squirrel_monkey_species.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Proboscis Monkey",
+      photo:
+        "https://www.monkeyworlds.com/wp-content/uploads/Proboscis_monkey.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      //They really do enjoy bonding and spend all their free time with each other
+      name: "Pygmy Marmoset",
+      photo:
+        "https://www.monkeyworlds.com/wp-content/uploads/pygmy.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    }
+  ];
+  module.exports = friends;
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(port, () => console.log("Listening on port %s", port));
